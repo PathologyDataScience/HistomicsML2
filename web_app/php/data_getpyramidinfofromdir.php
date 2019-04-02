2315aa613d163dede228c1f75e917044d985f2a2
slee172 <shlee8020@gmail.com>
2019-04-01 22:59:58 -0400

    Added data related php files modified

--- web_app/php/data_getpyramidinfofromdir.php
+++ web_app/php/data_getpyramidinfofromdir.php
@@ -1,7 +1,7 @@
 <?php
 
 //
-//	Copyright (c) 2014-2017, Emory University
+//	Copyright (c) 2014-2019, Emory University
 //	All rights reserved.
 //
 //	Redistribution and use in source and binary forms, with or without modification, are
