3a7f5fc23f6d954ce6197263849107c2ebbed09c
slee172 <shlee8020@gmail.com>
2019-05-05 14:33:42 -0400

    Modified variable for localhost

--- web_app/db/connect.php
+++ web_app/db/connect.php
@@ -49,7 +49,7 @@ function loggerConnect() {
 	require 'accounts.php';		// $logAccount $logPass $guestAccount $guestPass
 
 
-	$dbConn = mysqli_connect("localhost", $logAccount, $logPass, "nuclei");
+	$dbConn = mysqli_connect($dbAddress, $logAccount, $logPass, "nuclei");
 	if( !$dbConn ) {
 		echo("<p>Unable to connect to the database server</p>" . mysqli_connect_error() );
 		exit();
