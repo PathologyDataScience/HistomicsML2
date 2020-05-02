27243babed866922038d432d401a5de6b1a9397f
Sanghoon Lee <leesan@leesan-mac.local>
2020-05-02 10:52:48 -0400

    Updated web_app

--- web_app/db/getTrainsetForDataset.php
+++ web_app/db/getTrainsetForDataset.php
@@ -1,44 +1,17 @@
 <?php
 
-//
-//	Copyright (c) 2014-2015, Emory University
-//	All rights reserved.
-//
-//	Redistribution and use in source and binary forms, with or without modification, are
-//	permitted provided that the following conditions are met:
-//
-//	1. Redistributions of source code must retain the above copyright notice, this list of
-//	conditions and the following disclaimer.
-//
-//	2. Redistributions in binary form must reproduce the above copyright notice, this list 
-// 	of conditions and the following disclaimer in the documentation and/or other materials
-//	provided with the distribution.
-//
-//	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
-//	EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-//	OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
-//	SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
-//	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-//	BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-//	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
-//	WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-//	DAMAGE.
-//
-//
-
 	require 'logging.php';		// Also includes connect.php
 	require '../php/hostspecs.php';
 
 	$dataset = $_POST['dataset'];
-	
+
 //	$sql = 'SELECT t.name, t.fileName FROM training_sets t '
 //			.'JOIN datasets d ON t.dataset_id=d.id WHERE d.name="'.$dataset.'"';
 
-	$sql = 'SELECT name, fileName FROM training_sets';
-	
+	$sql = 'SELECT name, fileName, modelName FROM training_sets';
+
 	$dbConn = guestConnect();
-	
+
 	if( $result = mysqli_query($dbConn, $sql) ) {
 
 		$trainingSetNames = array();
@@ -46,9 +19,10 @@
 		while( $array = mysqli_fetch_row($result) ) {
 			$trainingSetNames[] = $array[0];
 			$fileNames[] = $array[1];
+			$modelNames[] = $array[2];
 		}
-		
-		$trainingSetData = array("trainingSets" => $trainingSetNames, "fileNames" => $fileNames);
+
+		$trainingSetData = array("trainingSets" => $trainingSetNames, "fileNames" => $fileNames, "modelNames" => $modelNames));
 		mysqli_free_result($result);
 
 	} else {
@@ -59,4 +33,3 @@
 
 	echo json_encode($trainingSetData);
 ?>
-
--- web_app/php/reloadSession_nn.php
+++ web_app/php/reloadSession_nn.php
@@ -1,32 +1,5 @@
 <?php
 
-//
-//	Copyright (c) 2014-2019, Emory University
-//	All rights reserved.
-//
-//	Redistribution and use in source and binary forms, with or without modification, are
-//	permitted provided that the following conditions are met:
-//
-//	1. Redistributions of source code must retain the above copyright notice, this list of
-//	conditions and the following disclaimer.
-//
-//	2. Redistributions in binary form must reproduce the above copyright notice, this list
-// 	of conditions and the following disclaimer in the documentation and/or other materials
-//	provided with the distribution.
-//
-//	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
-//	EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-//	OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
-//	SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-//	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-//	BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-//	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
-//	WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-//	DAMAGE.
-//
-//
-
 	require '../db/logging.php';
 
 	// Generate a unique id to track this session in the server
@@ -36,7 +9,7 @@
 	// Get the trainingset file from the database
 	//
 	$dbConn = guestConnect();
-	$sql = 'SELECT filename FROM training_sets WHERE name="'.$_POST["trainingset"].'"';
+	$sql = 'SELECT filename, modelname FROM training_sets WHERE name="'.$_POST["trainingset"].'"';
 
 	if( $result = mysqli_query($dbConn, $sql) ) {
 
@@ -61,6 +34,9 @@
 
 	write_log("INFO", "Session '".$_POST["trainingset"]."' reloaded");
 
+	// $command = escapeshellcmd('python ../../predict-rest-api/run_model_server.py');
+  // $output = shell_exec($command);
+
 	session_start();
 	$_SESSION['uid'] = $UID;
 	$_SESSION['classifier'] = $_POST["trainingset"];
@@ -71,6 +47,7 @@
 	$_SESSION['datapath'] = $featureFile[0];
 	$_SESSION['pcapath'] = $featureFile[1];
 	$_SESSION['trainingSetName'] = $filename[0];
+	$_SESSION['trainingSetModelName'] = $filename[1];
 	$_SESSION['reloaded'] = true;
 	$_SESSION['init_reloaded'] = true;
 	$_SESSION['superpixelSize'] = $featureFile[2];
