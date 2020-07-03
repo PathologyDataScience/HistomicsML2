82d6fdddf019d9840707936cfd6d334fce331581
Sanghoon Lee <leesan@leesan-mac-2.local>
2020-07-03 12:06:33 -0400

    Integrated dataset directory paths

--- predict-rest-api/count.py
+++ predict-rest-api/count.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import settings
 
 class count():
 	def __init__(self):
@@ -11,9 +11,11 @@ class count():
 		self.outFile = None
 
 	def setData(self, q):
+		# initialize settings
+		set = settings.Settings()
 		self.uid = q["uid"]
-		self.trainSet = '/datasets/classifiers/' + str(q["trainset"])
+		self.trainSet = set.TRAININGSET_DIR + str(q["trainset"])
 		self.classifier = str(q["trainset"])
 		self.dataSet = str(q["dataset"])
-		self.inFile = '/datasets/classifiers/tmp/' + str(q["trainset"]) + '_' + self.dataSet.split('/')[1] + '.csv'
-		self.outFile = 'trainingsets/tmp/' + str(q["trainset"]) + '_' + self.dataSet.split('/')[1] + '.csv'
+		self.inFile = set.TRAININGTEMP_DIR + str(q["trainset"]) + '_' + self.dataSet.split('/')[1] + '.csv'
+		self.outFile = set.SOFT_TRAININGTEMP_DIR + str(q["trainset"]) + '_' + self.dataSet.split('/')[1] + '.csv'
--- predict-rest-api/mapping.py
+++ predict-rest-api/mapping.py
@@ -1,5 +1,6 @@
 import numpy as np
 import dataset
+import settings
 
 class map():
 	def __init__(self):
@@ -12,10 +13,12 @@ class map():
 		self.outFile = None
 
 	def setData(self, q):
+		# initialize settings
+		set = settings.Settings()
 		self.uid = q["uid"]
 		self.slide = str(q["slide"])
-		self.trainSet = '/datasets/classifiers/' + str(q["trainset"])
+		self.trainSet = set.TRAININGSET_DIR + str(q["trainset"])
 		self.classifier = str(q["trainset"])
 		self.dataSet = str(q["dataset"])
-		self.inFile = '/datasets/classifiers/tmp/' + self.slide + '_' + str(q["trainset"]) + '_' + self.dataSet.split('/')[1] + '.h5'
-		self.outFile = 'trainingsets/tmp/' + self.slide + '_' + str(q["trainset"]) + '_' + self.dataSet.split('/')[1] + '.h5'
+		self.inFile = set.TRAININGTEMP_DIR + self.slide + '_' + str(q["trainset"]) + '_' + self.dataSet.split('/')[1] + '.h5'
+		self.outFile = set.SOFT_TRAININGTEMP_DIR + self.slide + '_' + str(q["trainset"]) + '_' + self.dataSet.split('/')[1] + '.h5'
